cd89b1df1f4c4d975ccd094a361cb4f931af047f
Stephen Bowen <bowensteven17@gmail.com>
2024-03-21 13:24:23 +0300

    LAB

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName ='bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'tom'
+
+function upperCaseCustomerName(customerName){
+    let newCustomerName = customerName;
+    return newCustomerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'John';
+}
+console.log(upperCaseCustomerName(customerName));
